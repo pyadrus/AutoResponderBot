dabdcd0ad8e722aa42315014e22d5afb2d6b72ed
@PyAdminRU <zh.vitaliy92@gmail.com>
2025-05-02 21:37:24 +0300

    Рефакторинг кода

--- configs/configs.py
+++ configs/configs.py
@@ -8,60 +8,39 @@ load_dotenv()
 
 
 def get_telegram_admin_id() -> str:
-    admin_id = os.getenv("ADMIN_ID")
-    if not admin_id:
-        raise ValueError("ADMIN_ID не найден в переменных окружения.")
-    return admin_id
+    """Возвращает ID администратора Telegram бота."""
+    return os.getenv("ADMIN_ID")
 
 
-# Функции для получения переменных окружения
 def get_groq_api_key() -> str:
     """Возвращает API-ключ для Groq."""
-    api_key = os.getenv("GROQ_API_KEY")
-    if not api_key:
-        raise ValueError("GROQ_API_KEY не найден в переменных окружения.")
-    return api_key
+    return os.getenv("GROQ_API_KEY")
 
 
 def get_telegram_bot_token() -> str:
     """Возвращает токен Telegram бота."""
-    token = os.getenv("TELEGRAM_BOT_TOKEN")
-    if not token:
-        raise ValueError("TELEGRAM_BOT_TOKEN не найден в переменных окружения.")
-    return token
+    return os.getenv("TELEGRAM_BOT_TOKEN")
 
 
 # Настройки прокси
 def get_proxy_user() -> str:
     """Возвращает логин для прокси."""
-    user = os.getenv("USER")
-    if not user:
-        raise ValueError("USER (логин прокси) не найден в переменных окружения.")
-    return user
+    return os.getenv("USER")
 
 
 def get_proxy_password() -> str:
     """Возвращает пароль для прокси."""
-    password = os.getenv("PASSWORD")
-    if not password:
-        raise ValueError("PASSWORD (пароль прокси) не найден в переменных окружения.")
-    return password
+    return os.getenv("PASSWORD")
 
 
 def get_proxy_port() -> str:
     """Возвращает порт для прокси."""
-    port = os.getenv("PORT")
-    if not port:
-        raise ValueError("PORT (порт прокси) не найден в переменных окружения.")
-    return port
+    return os.getenv("PORT")
 
 
 def get_proxy_ip() -> str:
     """Возвращает IP для прокси."""
-    ip = os.getenv("IP")
-    if not ip:
-        raise ValueError("IP (адрес прокси) не найден в переменных окружения.")
-    return ip
+    return os.getenv("IP")
 
 
 if __name__ == '__main__':
